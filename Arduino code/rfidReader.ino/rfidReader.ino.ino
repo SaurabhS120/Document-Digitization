17f9b9c41a7ef955a84e63bbe4a660fb23960bde
SaurabhS120 <Saurabh.sonar120@gmail.com>
2021-04-04 11:29:07 +0530

    on off led with start and stop command

--- Arduino code/rfidReader.ino/rfidReader.ino.ino
+++ Arduino code/rfidReader.ino/rfidReader.ino.ino
@@ -1,9 +1,39 @@
+#define LED 13
+#define SCAN true
+#define IDLE false
+String a;
+bool state=IDLE;
+void changeState(bool s){
+  state=s;
+  if(s==SCAN){
+    digitalWrite(LED,HIGH);
+  }else{
+    digitalWrite(LED,LOW);
+  }
+}
 void setup() {
-  // put your setup code here, to run once:
 
+Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
+pinMode(LED,OUTPUT);
+changeState(IDLE);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+while(Serial.available()) {
+
+a= Serial.readString();// read the incoming data as string
+
+Serial.println(a);
+a.trim();
+if(a.equals("start")){
+  changeState(SCAN);
+}
+if(a.equals("stop")){
+  changeState(IDLE);
+  
+}
+
+}
 
 }
